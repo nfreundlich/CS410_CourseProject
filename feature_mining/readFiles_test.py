7a286c7cb2878cfd447b8b79c43b1284b1b19da9
hwilder3 <hwilder3@illinois.edu>
2018-11-08 23:06:02 -0500

    Added two functions: format_feature_list - takes in a nested list and returns a data frame with ids for each feature
    read_annotated_data - reads in Santu's annotated input files, returns DataFrames with the annotation information as well as a somewhat cleaned set of section text, including ids - can be passed into the build_models function

--- feature_mining/readFiles_test.py
+++ feature_mining/readFiles_test.py
@@ -164,14 +164,73 @@ with open('docCounts.data', 'rb') as filehandle:
     filehandle.close()
 
 
-def build_explicit_models(text_set: object, feature_set: object, feature_mapping: object) -> object:
+
+
+
+
+
+
+def format_feature_list(feature_list: list) -> object:
+    """
+    This function takes a list of strings and/or lists of strings and converts them to a DataFrame with ids. Terms in
+    nested lists will be treated as synonyms and given the same feature id
+
+    :param feature_list: a list of strings and lists of strings. Individual strings will be given separate ids, lists
+    of strings will be treated as synonyms and given the same feature id.
+    ex. ["sound", "battery", ["screen", "display"]]
+    :return: DataFrame with integer ids for each feature, synonyms are grouped together
+    | feature (str) | feature_id (int)  | feature_term_id (int)
+    feature: string representation of the feature
+    feature_id: integer id for the feature, will be the same for synonyms if input in nested list
+    feature_term_id: integer id for the feature, will be unique for each string, including synonyms
+
+    """
+
+    feature_index = 0
+    feature_term_index = 0
+    rows_list = []
+
+    # loop through list of features
+    for feature in feature_list:
+        if isinstance(feature, str):
+            print("string")
+            rows_list.append({"feature_term_id": feature_term_index, "feature_id": feature_index, "feature": feature})
+            feature_term_index+=1
+        elif isinstance(feature, list):
+            print('list')
+            for subfeature in feature:
+                if isinstance(subfeature, str):
+                    print('>string')
+                    rows_list.append({"feature_term_id": feature_term_index, "feature_id": feature_index, "feature": subfeature})
+                    feature_term_index += 1
+                else:
+                    raise ValueError(str(feature) + '>' + str(subfeature) + ' is not a string or a list of strings')
+
+        else:
+            raise ValueError(str(feature)+ ' is not a string or a list of strings')
+
+        feature_index += 1
+
+    feature_df = pd.DataFrame(rows_list)
+
+    return feature_df
+
+
+format_feature_list(feature_list = ["sound", "battery", ["screen", "display"]])
+
+
+def build_explicit_models(text_set: object, feature_set: object) -> object:
     """
     This function builds a background model, set of topic models and summarizes the counts of words in each sentence
         to prepare for EM optimization
 
-    :param text_set: a pandas dataframe with (at a minimum) the following columns
-    :param feature_set:
-    :param feature_mapping:
+    :param text_set: a pandas DataFrame with (at a minimum) the following columns
+    :param feature_set: output of format feature list - OR -
+        DataFrame with integer ids for each feature, synonyms are grouped together
+        | feature (str) | feature_id (int)  | feature_term_id (int)
+        feature: string representation of the feature
+        feature_id: integer id for the feature, will be the same for synonyms if input in nested list
+        feature_term_id: integer id for the feature, will be unique for each string, including synonyms
     :return: a dictionary with three entries -
         model_background: background model estimated from the entire document collection as described in section 4.2
         model_topic: topic models estimated from explicit mention sentences as described in section 4.2
@@ -182,6 +241,7 @@ def build_explicit_models(text_set: object, feature_set: object, feature_mapping
     doc_counts_all = Counter()  # count of all words in all sentences
     sentence_counter = Counter()  # count of number of sentences with word
     feature_counter = defaultdict(Counter)
+    feature_mapping = []
 
     # loop over all rows in input data set
     for index, row in text_set.iterrows():
@@ -192,6 +252,8 @@ def build_explicit_models(text_set: object, feature_set: object, feature_mapping
         doc = nlp(row["text"])
 
         # add each parsed word into a list via list comprehension
+        # Note: won't catch capitalized stopwords, need to lowercase as part of pre-processing - also possible stop word
+        # filtering not necessary because of tfidf term in topic model?
         doc_word_list = []
         for word in doc:
             if not word.is_stop and not word.is_punct:
@@ -213,10 +275,14 @@ def build_explicit_models(text_set: object, feature_set: object, feature_mapping
         doc_list[row["sentence_id"]] = doc_word_list
 
         # get all explicit topics for this sentence and add these words to the list
-        sentence_features = feature_mapping.loc[feature_mapping['sentence_id'] == int(row["sentence_id"])]
-        if len(sentence_features.index) > 0:
-            for index_f, row_f in sentence_features.iterrows():
+        for index_f, row_f in feature_set.iterrows():
+
+            # word was found in the section, record find and add words to feature topic model
+            if row_f["feature"] in doc_word_list:
                 print("feature " + str(row_f["feature_id"]))
+
+                # record that feature was explicitly found
+                feature_mapping.append({"sentence_id": row["sentence_id"] ,"feature_id": row_f["feature_id"]})
 
                 # if we only count each word once
                 feature_counter[row_f["feature_id"]].update(word_counts.keys())
@@ -276,4 +342,98 @@ def build_explicit_models(text_set: object, feature_set: object, feature_mapping
     return model_results
 
 
-em_input = build_explicit_models(text_set=text_set, feature_set=feature_set, feature_mapping=feature_mapping)
+em_input = build_explicit_models(text_set=text_set, feature_set=feature_set)
+
+
+def read_annotated_data(filename: str, nlines=None) -> object:
+    """
+    Reads in Santu's annotated files and records the explicit features and implicit features annotated in the file
+    :param filename: Filename for the annotated data set
+    :param nlines: Maximum number of lines from the file to read or None to read all lines
+    :return: a dictionary with the following data
+        section_list: DataFrame with the following form
+            | doc_id (int)  | section_id (int)  | section_text (str)    | title (bool)  |
+            doc_id: integer id for the document
+            section_id: integer id for the section
+            section_text: cleaned (lowercase, trimmed) section text
+            title: True if the line is a title, False otherwise
+        feature_mapping: DataFrame
+            | doc_id (int)  | feature (str) | is_explicit (bool)    | section_id (int)  |
+            doc_id: integer id for the document
+            feature: the string form of the feature in the annotation
+            is_explicit: False if the feature was marked in the annotation as an implicit mention, True otherwise
+            section_id: integer id for the section
+        feature_list: dictionary with each feature and the number of sections it appears in
+            key: feature name
+            value: number of sections in which the feature appears
+    """
+
+    doc_id=-1
+    section_id=0
+    section_list = []
+    feature_mapping = []
+    feature_list = defaultdict(int)
+    with open(filename,'r') as input_file:
+        for line in input_file:
+
+            # Section is from new doc, increment doc id
+            if '[t]' in line:
+                doc_id += 1
+                is_title=True
+                line_text = line.split('[t]')[1].strip().lower()
+
+            # Section is from new doc, increment doc id
+            elif line.startswith('*'):
+                doc_id += 1
+                is_title=True
+                line_text = line.split('*')[1].strip().lower()
+
+            # Section not from new doc, just get cleaned text
+            else:
+                is_title=False
+                line_text=line.split('##')[1].strip().lower()
+
+            # Look for feature annotations attached to the line
+            feature_string = line.split('##')[0].split(',')
+            if feature_string[0] != '':
+
+                # Loop through all the features found in the annotation
+                for feature in feature_string:
+                    # print(feature)
+
+                    # Check if the feature in the annotation is marked as an implicit mention
+                    if '[u]' in feature:
+                        explicit_feature=False
+                        # print('implicit')
+                    else:
+                        explicit_feature=True
+
+                    # Get the actual text of the feature
+                    feature_text = feature.split('[@]')[0]
+
+                    # Add the feature and section id to the data set
+                    feature_mapping.append({"doc_id": doc_id, "section_id": section_id, "feature": feature_text,
+                                            "is_explicit": explicit_feature})
+
+                # Increment the feature in the unique feature list
+                feature_list[feature_text]+=1
+
+            # Add section line to data set
+            section_list.append(
+                {"doc_id": doc_id, "section_id": section_id, "section_text": line_text, "title": is_title})
+
+            # Increment section id
+            section_id+=1
+            #print(line)
+
+            # Check if max number of lines has been reached yet
+            if not nlines is None:
+                if section_id>=nlines:
+                    break
+
+    #Bundle and return data set
+    return dict(section_list=pd.DataFrame(section_list), feature_mapping=pd.DataFrame(feature_mapping),
+                feature_list=feature_list)
+
+
+annotated_data = read_annotated_data(filename = 'demo_files/iPod.final', nlines = 100)

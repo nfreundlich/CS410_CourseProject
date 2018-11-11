0db879ae2b754e5db4e884581031d0406dfb59e0
hewilder <hwilder3@illinois.edu>
2018-11-10 19:05:35 -0500

    Recommitting - didn't get contents of parse_and_model the first time for some reason
    Cleaned up code (renamed based on agreed upon collection > document > section > word naming convention)
    
    Moved into separate file
    
    Still to do:
     - Performance issues in model building function, need to investigate and fix
     - Reformat version of output for model building into matrices to be passed off to EM

--- feature_mining/readFiles_test.py
+++ feature_mining/readFiles_test.py
@@ -136,7 +136,7 @@ with open('model_background.data', 'wb') as filehandle:
     pickle.dump(model_background, filehandle)
     filehandle.close()
 
-with open('model_topic.data', 'wb') as filehandle:
+with open('model_feature.data', 'wb') as filehandle:
     # store the data as binary data stream
     pickle.dump(model_topic, filehandle)
     filehandle.close()
@@ -153,7 +153,7 @@ with open('model_background.data', 'rb') as filehandle:
     rd_model_background = pickle.load(filehandle)
     filehandle.close()
 
-with open('model_topic.data', 'rb') as filehandle:
+with open('model_feature.data', 'rb') as filehandle:
     # store the data as binary data stream
     rd_model_topic = pickle.load(filehandle)
     filehandle.close()
@@ -164,16 +164,12 @@ with open('docCounts.data', 'rb') as filehandle:
     filehandle.close()
 
 
-
-
-
-
-
-
-def format_feature_list(feature_list: list) -> object:
+def format_feature_list(feature_list: list) -> pd.DataFrame:
     """
     This function takes a list of strings and/or lists of strings and converts them to a DataFrame with ids. Terms in
     nested lists will be treated as synonyms and given the same feature id
+
+    ex. format_feature_list(feature_list = ["sound", "battery", ["screen", "display"]])
 
     :param feature_list: a list of strings and lists of strings. Individual strings will be given separate ids, lists
     of strings will be treated as synonyms and given the same feature id.
@@ -188,166 +184,42 @@ def format_feature_list(feature_list: list) -> object:
 
     feature_index = 0
     feature_term_index = 0
-    rows_list = []
+    formatted_feature_list = []
 
     # loop through list of features
     for feature in feature_list:
         if isinstance(feature, str):
             print("string")
-            rows_list.append({"feature_term_id": feature_term_index, "feature_id": feature_index, "feature": feature})
+            formatted_feature_list.append(
+                {"feature_term_id": feature_term_index, "feature_id": feature_index, "feature": feature})
             feature_term_index+=1
         elif isinstance(feature, list):
             print('list')
-            for subfeature in feature:
-                if isinstance(subfeature, str):
+            for synonym in feature:
+                if isinstance(synonym, str):
                     print('>string')
-                    rows_list.append({"feature_term_id": feature_term_index, "feature_id": feature_index, "feature": subfeature})
+                    formatted_feature_list.append(
+                        {"feature_term_id": feature_term_index, "feature_id": feature_index, "feature": synonym})
                     feature_term_index += 1
                 else:
-                    raise ValueError(str(feature) + '>' + str(subfeature) + ' is not a string or a list of strings')
+                    raise ValueError(str(feature) + '>' + str(synonym) + ' is not a string or a list of strings')
 
         else:
             raise ValueError(str(feature)+ ' is not a string or a list of strings')
 
         feature_index += 1
 
-    feature_df = pd.DataFrame(rows_list)
+    feature_df = pd.DataFrame(formatted_feature_list)
 
     return feature_df
 
 
-format_feature_list(feature_list = ["sound", "battery", ["screen", "display"]])
-
-
-def build_explicit_models(text_set: object, feature_set: object) -> object:
-    """
-    This function builds a background model, set of topic models and summarizes the counts of words in each sentence
-        to prepare for EM optimization
-
-    :param text_set: a pandas DataFrame with (at a minimum) the following columns
-    :param feature_set: output of format feature list - OR -
-        DataFrame with integer ids for each feature, synonyms are grouped together
-        | feature (str) | feature_id (int)  | feature_term_id (int)
-        feature: string representation of the feature
-        feature_id: integer id for the feature, will be the same for synonyms if input in nested list
-        feature_term_id: integer id for the feature, will be unique for each string, including synonyms
-    :return: a dictionary with three entries -
-        model_background: background model estimated from the entire document collection as described in section 4.2
-        model_topic: topic models estimated from explicit mention sentences as described in section 4.2
-        doc_counts: word counts in each sentence as needed by the EM algorithm
-    """
-    doc_list = dict()  # list of all terms in each sentence
-    doc_counts = dict()  # count of terms in each sentence
-    doc_counts_all = Counter()  # count of all words in all sentences
-    sentence_counter = Counter()  # count of number of sentences with word
-    feature_counter = defaultdict(Counter)
-    feature_mapping = []
-
-    # loop over all rows in input data set
-    for index, row in text_set.iterrows():
-        # print the current text for debugging
-        print(str(row["sentence_id"]) + ":" + row["text"])
-
-        # input the sentence into Spacy
-        doc = nlp(row["text"])
-
-        # add each parsed word into a list via list comprehension
-        # Note: won't catch capitalized stopwords, need to lowercase as part of pre-processing - also possible stop word
-        # filtering not necessary because of tfidf term in topic model?
-        doc_word_list = []
-        for word in doc:
-            if not word.is_stop and not word.is_punct:
-                doc_word_list.append(word.lemma_ if word.lemma_ != '-PRON-' else word.lower_)
-
-        # get a count of distinct words in the doc - this might need to be switched to default dict later
-        word_counts = Counter(doc_word_list)
-
-        # get keys for distinct words to add to idf counter
-        sentence_counter.update(word_counts.keys())
-
-        # add these counts to the all doc counter
-        doc_counts_all.update(doc_word_list)
-
-        # add to doc counts dictionary
-        doc_counts[row["sentence_id"]] = word_counts
-
-        # add to dictionary holding word parsing
-        doc_list[row["sentence_id"]] = doc_word_list
-
-        # get all explicit topics for this sentence and add these words to the list
-        for index_f, row_f in feature_set.iterrows():
-
-            # word was found in the section, record find and add words to feature topic model
-            if row_f["feature"] in doc_word_list:
-                print("feature " + str(row_f["feature_id"]))
-
-                # record that feature was explicitly found
-                feature_mapping.append({"sentence_id": row["sentence_id"] ,"feature_id": row_f["feature_id"]})
-
-                # if we only count each word once
-                feature_counter[row_f["feature_id"]].update(word_counts.keys())
-
-                # if we count each words as many times as it occurs
-                # featureCounter[row_f["feature_id"]].update(doc_word_list)
-
-    # At this point we have all the counts we need to build the topic models
-
-    ####################################
-    # Calculations for background model
-    ####################################
-
-    # total number of words
-    total_word_count = sum(doc_counts_all.values())
-
-    # change counter to dictionary for calculations
-    doc_counts_all_dict = dict(doc_counts_all)
-
-    # calculate background model
-    model_background = dict((k, v / total_word_count) for k, v in doc_counts_all_dict.items())
-
-    ###############################
-    # Calculations for topic model
-    ###############################
-    tfidf_topic = defaultdict(dict)
-    model_topic_norms = Counter()
-
-    # count of sentences
-    num_sentences = len(doc_list)
-    num_words = len(doc_counts_all_dict)
-
-    for word in doc_counts_all_dict.keys():
-        print(word)
-
-        for index, row in feature_set.iterrows():
-            print(str(index) + "-" + row["feature"])
-
-            tfidf = math.log(1 + feature_counter[index][word]) * math.log(
-                1 + num_sentences / sentence_counter[word]) + 1
-            print(str(tfidf))
-
-            tfidf_topic[index][word] = tfidf
-
-            model_topic_norms[index] += tfidf
-
-    # normalize values of all dictionaries with totals
-    model_topic = defaultdict(dict)
-
-    for index in model_topic_norms.keys():
-        print("normalizing " + str(index))
-
-        model_topic[index] = dict((k, v / (model_topic_norms[index])) for k, v in tfidf_topic[index].items())
-
-    model_results = dict(model_background=model_background, model_topic=model_topic, doc_counts=doc_counts)
-
-    return model_results
-
-
-em_input = build_explicit_models(text_set=text_set, feature_set=feature_set)
-
-
-def read_annotated_data(filename: str, nlines=None) -> object:
+def read_annotated_data(filename: str, nlines: int =None) -> dict:
     """
     Reads in Santu's annotated files and records the explicit features and implicit features annotated in the file
+
+    ex. annotated_data = read_annotated_data(filename='demo_files/iPod.final', nlines=200)
+
     :param filename: Filename for the annotated data set
     :param nlines: Maximum number of lines from the file to read or None to read all lines
     :return: a dictionary with the following data
@@ -357,7 +229,7 @@ def read_annotated_data(filename: str, nlines=None) -> object:
             section_id: integer id for the section
             section_text: cleaned (lowercase, trimmed) section text
             title: True if the line is a title, False otherwise
-        feature_mapping: DataFrame
+        feature_section_mapping: DataFrame
             | doc_id (int)  | feature (str) | is_explicit (bool)    | section_id (int)  |
             doc_id: integer id for the document
             feature: the string form of the feature in the annotation
@@ -371,10 +243,13 @@ def read_annotated_data(filename: str, nlines=None) -> object:
     doc_id=-1
     section_id=0
     section_list = []
-    feature_mapping = []
+    feature_section_mapping = []
     feature_list = defaultdict(int)
+
     with open(filename,'r') as input_file:
         for line in input_file:
+
+            print(line)
 
             # Section is from new doc, increment doc id
             if '[t]' in line:
@@ -395,7 +270,8 @@ def read_annotated_data(filename: str, nlines=None) -> object:
 
             # Look for feature annotations attached to the line
             feature_string = line.split('##')[0].split(',')
-            if feature_string[0] != '':
+            print(feature_string)
+            if not is_title and feature_string[0] != '':
 
                 # Loop through all the features found in the annotation
                 for feature in feature_string:
@@ -412,28 +288,170 @@ def read_annotated_data(filename: str, nlines=None) -> object:
                     feature_text = feature.split('[@]')[0]
 
                     # Add the feature and section id to the data set
-                    feature_mapping.append({"doc_id": doc_id, "section_id": section_id, "feature": feature_text,
-                                            "is_explicit": explicit_feature})
+                    feature_section_mapping.append({"doc_id": doc_id, "section_id": section_id, "feature": feature_text,
+                                                    "is_explicit": explicit_feature})
 
-                # Increment the feature in the unique feature list
-                feature_list[feature_text]+=1
+                    # Increment the feature in the unique feature list
+                    feature_list[feature_text] += 1
 
             # Add section line to data set
             section_list.append(
                 {"doc_id": doc_id, "section_id": section_id, "section_text": line_text, "title": is_title})
 
             # Increment section id
-            section_id+=1
-            #print(line)
+            section_id += 1
+            # print(line)
 
             # Check if max number of lines has been reached yet
-            if not nlines is None:
-                if section_id>=nlines:
+            if nlines is not None:
+                if section_id >= nlines:
                     break
 
-    #Bundle and return data set
-    return dict(section_list=pd.DataFrame(section_list), feature_mapping=pd.DataFrame(feature_mapping),
+    # Bundle and return data set
+    return dict(section_list=pd.DataFrame(section_list), feature_mapping=pd.DataFrame(feature_section_mapping),
                 feature_list=feature_list)
 
 
-annotated_data = read_annotated_data(filename = 'demo_files/iPod.final', nlines = 100)
+def build_explicit_models(text_set: pd.DataFrame, feature_set: pd.DataFrame) -> dict:
+    """
+    This function builds a background model, set of topic models and summarizes the counts of words in each sentence
+        to prepare for EM optimization
+
+        ex. em_input = build_explicit_models(text_set=text_set, feature_set=feature_set)
+
+    :param text_set: a pandas DataFrame with (at a minimum) the following columns
+    :param feature_set: output of format feature list - OR -
+        DataFrame with integer ids for each feature, synonyms are grouped together
+        | feature (str) | feature_id (int)  | feature_term_id (int)
+        feature: string representation of the feature
+        feature_id: integer id for the feature, will be the same for synonyms if input in nested list
+        feature_term_id: integer id for the feature, will be unique for each string, including synonyms
+    :return: a dictionary with three entries -
+        model_background: background model estimated from the entire document collection as described in section 4.2
+        model_feature: feature models estimated from explicit mention sections as described in section 4.2
+        section_word_counts: word counts in each section as needed by the EM algorithm
+    """
+    section_word_list = dict()  # list of all words in each section
+    section_word_counts = dict()  # count of words in each section
+    collection_word_counts = Counter()  # count of all words in all section
+    word_section_counter = Counter()  # count of number of sections with word
+    feature_word_counter = defaultdict(Counter) # keep track of words appearing in section with explicit feature mention
+    feature_section_mapping = [] # keeps a list of the sentence ids associated with each feature (many-to-many mapping)
+
+    # loop over all rows in input data set
+    for index, row in text_set.iterrows():
+        # print the current text for debugging
+        print(str(row["sentence_id"]) + ":" + row["text"])
+
+        # input the sentence into Spacy
+        section = nlp(row["text"])
+
+        # add each parsed word into a list
+        # Note: won't catch capitalized stopwords, need to lowercase as part of pre-processing - also possible stop word
+        # filtering not necessary because of tfidf term in topic model?
+        current_section_words = []
+        for word in section:
+            if not word.is_stop and not word.is_punct:
+                current_section_words.append(word.lemma_ if word.lemma_ != '-PRON-' else word.lower_)
+
+        # get a count of distinct words in the section - this might need to be switched to default dict later
+        current_section_word_counts = Counter(current_section_words)
+
+        # get keys for distinct words to add to idf counter
+        word_section_counter.update(current_section_word_counts.keys())
+
+        # add these counts to the all section counter
+        collection_word_counts.update(current_section_words)
+
+        # add to section counts dictionary
+        section_word_counts[row["sentence_id"]] = current_section_word_counts
+
+        # add to dictionary holding word parsing
+        section_word_list[row["sentence_id"]] = current_section_words
+
+        # initialize list to keep track of found features (in case of synonyms)
+        found_features = set()
+
+        # get all explicit topics for this sentence and add these words to the list
+        for index_f, row_f in feature_set.iterrows():
+
+            # word was found in the section, record find and add words to feature topic model
+            if row_f["feature"] in current_section_words:
+                print("feature " + str(row_f["feature_id"]))
+
+                if row_f("feature_id") in found_features:
+                    # already found explicit feature mention in sentence as synonym, skip
+                    continue
+                else:
+                    # feature has not been found yet, add to the list
+                    found_features.add(row_f("feature_id"))
+
+                # record that feature was explicitly found
+                feature_section_mapping.append({"sentence_id": row["sentence_id"] ,"feature_id": row_f["feature_id"]})
+
+                # if we only count each word once
+                feature_word_counter[row_f["feature_id"]].update(current_section_word_counts.keys())
+
+                # if we count each words as many times as it occurs
+                # featureCounter[row_f["feature_id"]].update(current_section_words)
+
+    # At this point we have all the counts we need to build the topic models
+
+    ####################################
+    # Calculations for background model
+    ####################################
+
+    # total number of words
+    vocabulary_size = sum(collection_word_counts.values())
+
+    # change counter to dictionary for calculations
+    collection_word_counts = dict(collection_word_counts)
+
+    # calculate background model
+    model_background = dict((k, v / vocabulary_size) for k, v in collection_word_counts.items())
+
+    ###############################
+    # Calculations for topic model
+    ###############################
+    tfidf_feature = defaultdict(dict)
+    model_feature_norms = Counter()
+
+    # count of sentences
+    section_count = len(section_word_list)
+    # num_words = len(collection_word_counts)
+
+    for word in collection_word_counts.keys():
+        print(word)
+
+        for index, row in feature_set.iterrows():
+            print(str(index) + "-" + row["feature"])
+
+            ##########################################################################
+            # Formula 4, section 4.2, using base 2 logs, also adds +1 from Formula 5
+            #########################################################################
+            tfidf = math.log(1 + feature_word_counter[index][word], 2) \
+                * math.log(1 + section_count / word_section_counter[word], 2) \
+                + 1
+            print(str(tfidf))
+
+            tfidf_feature[index][word] = tfidf
+
+            model_feature_norms[index] += tfidf
+
+    # normalize values of all dictionaries with totals
+    model_feature = defaultdict(dict)
+
+    for index in model_feature_norms.keys():
+        print("normalizing " + str(index))
+
+        #########################################################################################################
+        # Formula 5, section 4.2, using base 2 logs, +1 in numerator already taken care of in tfidf calculation
+        #########################################################################################################
+        model_feature[index] = dict((k, v / (model_feature_norms[index])) for k, v in tfidf_feature[index].items())
+
+    model_results = dict(model_background=model_background, model_feature=model_feature, doc_counts=section_word_counts)
+
+    return model_results
+
+
+
